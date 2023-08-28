75f95938e599ae3e5154c508ab192c778e05d88a
Max_Shulga <shulgamackim@gmail.com>
2023-08-28 10:58:37 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true
+    });
+})
+$(document).ready(function () {
+    $('.slider-2').slick({
+        arrows: false,
+        dots: true
+    });
+})
